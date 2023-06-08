72731b232d99a56a31a5b6e3f7337d168c88341c
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-06-08 19:49:33 -0300

    [feat] add constructor

--- internal/domain/entity/balance.go
+++ internal/domain/entity/balance.go
@@ -5,3 +5,11 @@ type Balance struct {
 	UserID string
 	Amount float64
 }
+
+func NewBalance(id string, userID string, amount float64) *Balance {
+	return &Balance{
+		ID:     id,
+		UserID: userID,
+		Amount: amount,
+	}
+}
