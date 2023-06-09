f7d4fa0a302eea6321030a3299e1c2cf8227e63f
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-06-09 10:56:11 -0300

    [chore] add migration to insert more data

---
+++ sql/migrations/000003_insert_data.up.sql
@@ -0,0 +1,3 @@
+insert into balances (id, user_id, amount) values 
+('220b5328-c13e-46ba-ad26-f4ce0159723e', '756a0523-4ed5-4f40-844a-a8b7391626d8', 1000), 
+('fd71691a-6845-4203-9eea-e8fde9fd139a', '622548f1-ff02-45c2-b2f3-c3d7af5c0f1d', 1000);
