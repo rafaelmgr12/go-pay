3e4df86d7be538ca774cb6f40bea33b5d0beb609
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-06-09 11:16:39 -0300

    [feat] add validations rules

--- internal/usecase/transactions/transactions.go
+++ internal/usecase/transactions/transactions.go
@@ -2,6 +2,7 @@ package transactions
 
 import (
 	"context"
+	"errors"
 
 	"github.com/rafaelmgr12/go-pay/internal/domain/gateway"
 )
@@ -32,9 +33,34 @@ func (uc *TransactionsUseCase) GetAmountById(id string, ctx context.Context) (fl
 }
 
 func (uc *TransactionsUseCase) Transfer(debtorId string, creditorId string, amount float64, ctx context.Context) error {
+	errValidate := uc.validate(creditorId, debtorId, amount, ctx)
+	if errValidate != nil {
+		return errValidate
+	}
 	err := uc.BalanceGateway.Transfer(debtorId, creditorId, amount, ctx)
 	if err != nil {
 		return err
+	}
+
+	return nil
+}
+
+func (uc *TransactionsUseCase) validate(creditorId string, debtorId string, amount float64, ctx context.Context) error {
+	if creditorId == debtorId {
+		return errors.New("creditor_id and debtor_id must be different")
+	}
+
+	if amount <= 0 {
+		return errors.New("amount must be greater than 0")
+	}
+
+	debtorAmount, err := uc.BalanceGateway.GetAmountById(ctx, debtorId)
+	if err != nil {
+		return err
+	}
+
+	if debtorAmount < amount {
+		return errors.New("debtor does not have enough balance")
 	}
 
 	return nil
